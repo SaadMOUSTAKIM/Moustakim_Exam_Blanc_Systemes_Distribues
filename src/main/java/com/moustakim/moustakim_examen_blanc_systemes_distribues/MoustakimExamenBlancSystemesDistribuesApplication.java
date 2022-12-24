972b9b10aa0223945305f8a47eeef4e7c29acebb
Saad MOUSTAKIM <saadmoustakim2@gmail.com>
2022-12-24 18:48:59 +0100

    First

---
+++ src/main/java/com/moustakim/moustakim_examen_blanc_systemes_distribues/MoustakimExamenBlancSystemesDistribuesApplication.java
@@ -0,0 +1,13 @@
+package com.moustakim.moustakim_examen_blanc_systemes_distribues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoustakimExamenBlancSystemesDistribuesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoustakimExamenBlancSystemesDistribuesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moustakim/moustakim_examen_blanc_systemes_distribues/MoustakimExamenBlancSystemesDistribuesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moustakim.moustakim_examen_blanc_systemes_distribues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoustakimExamenBlancSystemesDistribuesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
